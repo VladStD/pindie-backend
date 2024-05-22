8b39dd88db3e02b32e5599f24e5dc8ec688da50d
VladStD <vladstcloud@mail.ru>
2024-05-22 19:08:46 +0300

    Финальное ревью

---
+++ README.md
@@ -0,0 +1,5 @@
+Ссылка на репозиторий с фронтендом: https://github.com/VladStD/pindie-frontend
+
+Ссылка на домен фронтенда: vladst-frontend.nomoredomainswork.ru
+
+IP-адрес: <158.160.169.109>
--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,19 +3,19 @@ const allowedCors = [
     'https://students-projects.ru',
     'http://localhost:3000',
     'http://localhost:3001',
-    'http://localhost:1488'
+    'http://localhost:1488',
+    'https://vladst-frontend.nomoredomainswork.ru'
   ];
 
   function cors(req, res, next) {
     const { origin } = req.headers;
     
-    if (allowedCors.includes(origin)) { // Если это наш друг
-        res.header('Access-Control-Allow-Origin', origin);
-        res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");  
+    if (allowedCors.includes(origin)) {
+      res.header("Access-Control-Allow-Origin", origin);
     }
-    
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next();
-}
+  }
 
 module.exports = cors;
